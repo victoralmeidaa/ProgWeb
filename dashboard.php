b1a6db38d54238eef6ab5c29222f1cd5ad0c987b
Victor Almeida <victordealmeida5@gmail.com>
2025-03-21 15:17:40 -0300

    210320251517

--- dashboard.php
+++ dashboard.php
@@ -2,10 +2,10 @@
 session_start();
 
 if (!isset($_SESSION['usuario'])) {
-    header("Location: login.php");
+    header("Location: login.html");
     exit();
 }
 
 echo "<h2>Bem-vindo, " . $_SESSION['usuario'] . "!</h2>";
 echo "<a href='logout.php'>Sair</a>";
-?>
+?> 
